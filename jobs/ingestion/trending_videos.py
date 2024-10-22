3695efa4fd86902f02dbdeea696bae27a4a377b8
khanh <khanh28112001@gmail.com>
2024-10-22 16:26:15 +0700

    Create spark session manager

--- jobs/ingestion/trending_videos.py
+++ jobs/ingestion/trending_videos.py
@@ -10,6 +10,7 @@ from datetime import datetime
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, BooleanType, DoubleType, TimestampType
 from helper.logger import LoggerSimple
 
+
 load_dotenv()
 
 logger = LoggerSimple.get_logger(__name__)
@@ -34,30 +35,7 @@ class TrendingVideosFetcher(YoutubeFetcher):
 
 def fetch_and_save_trending_videos(current_date, batch_run_timestamp):
     try:
-
-        spark = SparkSession.builder \
-            .appName('Extract trending videos data. 📈') \
-            .master('local[*]') \
-            .config("spark.hadoop.fs.s3a.access.key", os.getenv("MINIO_ACCESS_KEY")) \
-            .config("spark.hadoop.fs.s3a.secret.key", os.getenv("MINIO_SECRET_KEY")) \
-            .config("spark.hadoop.fs.s3a.endpoint", os.getenv("MINIO_ENDPOINT")) \
-            .config("spark.hadoop.fs.s3a.path.style.access", "true")\
-            .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-            .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false")\
-            .config('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider')\
-            .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-            .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")\
-            .config('spark.sql.warehouse.dir', f's3a://{os.getenv("DATALAKE_BUCKET")}/')\
-            .config('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.3.4') \
-            .config('spark.jars.packages', 'com.amazonaws:aws-java-sdk-bundle:1.12.367') \
-            .config("spark.jars.packages", "io.delta:delta-core_2.12:2.4.0") \
-            .config('spark.driver.extraClassPath', '/opt/spark/jars/hadoop-aws-3.3.4.jar:/opt/spark/jars/s3-2.18.41.jar:/opt/spark/jars/aws-java-sdk-1.12.367.jar:/opt/spark/jars/delta-core_2.12-2.3.0.jar:/opt/spark/jars/delta-storage-2.2.0.jar')\
-            .config('spark.executor.extraClassPath', '/opt/spark/jars/hadoop-aws-3.3.4.jar:/opt/spark/jars/s3-2.18.41.jar:/opt/spark/jars/aws-java-sdk-1.12.367.jar:/opt/spark/jars/delta-core_2.12-2.3.0.jar:/opt/spark/jars/delta-storage-2.2.0.jar')\
-            .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-            .enableHiveSupport()\
-            .getOrCreate()
-
-        spark.sparkContext.setLogLevel("ERROR")
+        spark = SparkSessionManager.get_session()
 
         data_manager = BaseCSVManager(
             spark=spark,
@@ -75,6 +53,8 @@ def fetch_and_save_trending_videos(current_date, batch_run_timestamp):
 
     except Exception as e:
         logger.error(f"Error in fetching or saving trending videos: {str(e)}")
+    finally:
+        SparkSessionManager.close_session()
 
 
 if __name__ == "__main__":
