6dcfe88438d3d8b69414b1624cbeadd0491baae7
khanh <khanh28112001@gmail.com>
2024-11-23 20:33:13 +0700

    Working docker one

--- common/handle_scds.py
+++ common/handle_scds.py
@@ -31,10 +31,23 @@ class SCDHandler:
         data_format = output_config.get('format', 'parquet')
         mode = output_config.get('mode', 'overwrite')
 
-        df.write.format(data_format) \
-            .mode(mode) \
-            .partitionBy(partition_by) \
-            .save(output_path)
+        if DeltaTable.isDeltaTable(self.spark, output_path):
+            delta_table = DeltaTable.forPath(self.spark, output_path)
+        else:
+            df.write.format('delta').partitionBy(
+                partition_by).mode('overwrite').save(output_path)
+            return
+
+        merge_condition = ' AND '.join(
+            [f"tgt.{col} =src.{col}" for col in primary_key])
+
+        delta_table.alias("tgt").merge(
+            source=df.alias("src"),
+            condition=merge_condition
+        ) \
+            .whenMatchedUpdateAll() \
+            .whenNotMatchedInsertAll() \
+            .execute()
 
     def handle_scd2(self, df: DataFrame, output_config: Dict[str, Any]):
         """
@@ -51,13 +64,12 @@ class SCDHandler:
         if DeltaTable.isDeltaTable(self.spark, output_path):
             delta_table = DeltaTable.forPath(self.spark, output_path)
         else:
-
             df.limit(0).write.format('delta').save(output_path)
             delta_table = DeltaTable.forPath(self.spark, output_path)
 
         df_updates = df.withColumn('start_date', F.current_timestamp()) \
-                       .withColumn('end_date', F.lit(None).cast('timestamp')) \
-                       .withColumn('is_current', F.lit(True))
+            .withColumn('end_date', F.lit(None).cast('timestamp')) \
+            .withColumn('is_current', F.lit(True))
 
         merge_condition = ' AND '.join(
             [f"tgt.{col} = src.{col}" for col in primary_key])
@@ -74,7 +86,11 @@ class SCDHandler:
                 'end_date': F.current_timestamp(),
                 'is_current': F.lit(False)
             }
-        ).whenNotMatchedInsertAll().execute()
+        ).whenNotMatchedInsert(
+            values={
+                **{col: F.col(f"src.{col}") for col in df_updates.columns}
+            }
+        ).execute()
 
     def handle_scd3(self, df: DataFrame, output_config: Dict[str, Any]):
         """
@@ -84,28 +100,33 @@ class SCDHandler:
         primary_key = output_config['primary_key']
         tracking_columns = output_config.get('tracking_columns', [])
         partition_by = output_config.get('partition_by', [])
-        data_format = output_config.get('format', 'parquet')
-        mode = output_config.get('mode', 'overwrite')
+        data_format = output_config.get('format', 'delta')
 
-        if os.path.exists(output_path):
-            df_existing = self.spark.read.format(data_format).load(output_path)
+        if DeltaTable.isDeltaTable(self.spark, output_path):
+            delta_table = DeltaTable.forPath(self.spark, output_path)
+            df_existing = delta_table.toDF()
         else:
             df_existing = self.spark.createDataFrame([], df.schema)
 
-        join_condition = [df[col] == df_existing[col] for col in primary_key]
-        df_joined = df.join(df_existing, on=join_condition, how='left')
+        join_condition = ' AND ' \
+            .join([f"src.{col} = tgt.{col}" for col in primary_key])
+        df_joined = df.alias('src') \
+            .join(df_existing.alias('tgt'), on=primary_key, how='left')
 
         for col_name in tracking_columns:
-            df_joined = df_joined.withColumn(f'prev_{col_name}',
-                                             F.coalesce(df_existing[col_name], F.lit(None))) \
-                .withColumn(col_name, df[col_name])
+            df_joined = df_joined.withColumn(
+                f"prev_{col_name}", F.coalesce(
+                    F.col(f'tgt.{col_name}'), F.lit(None))
+            ).withColumn(
+                col_name, F.col(f"src.{col_name}")
+            )
 
         selected_columns = df.columns + \
-            [f'prev_{col}' for col in tracking_columns]
+            [f"prev_{col}" for col in tracking_columns]
         df_result = df_joined.select(*selected_columns)
 
         df_result.write.format(data_format) \
-            .mode(mode) \
+            .mode("overwrite") \
             .partitionBy(partition_by) \
             .save(output_path)
 
@@ -115,21 +136,35 @@ class SCDHandler:
         """
         current_table_path = output_config['path']
         history_table_path = output_config.get('history_path')
+        primary_key = output_config['primary_key']
         partition_by = output_config.get('partition_by', [])
-        data_format = output_config.get('format', 'parquet')
-        mode = output_config.get('mode', 'overwrite')
+        data_format = output_config.get('format', 'delta')
 
         if not history_table_path:
             raise ValueError(
                 "For SCD Type 4, 'history_path' must be specified in the output configuration.")
 
-        df.write.format(data_format) \
-            .mode(mode) \
-            .partitionBy(partition_by) \
-            .save(current_table_path)
+        if DeltaTable.isDeltaTable(self.spark, current_table_path):
+            current_table = DeltaTable.forPath(self.spark, current_table_path)
 
-        df_history = df.withColumn('record_timestamp', F.current_timestamp())
-        df_history.write.format(data_format) \
-            .mode('append') \
-            .partitionBy(partition_by) \
-            .save(history_table_path)
+            merge_condition = ' AND '.join(
+                [f"tgt.{col} = src.{col}" for col in primary_key])
+            current_table.alias("tgt").merge(
+                source=df.alias("src"),
+                condition=merge_condition
+            ).whenMatchedUpdateAll() \
+                .whenNotMatchedInsertAll() \
+                .execute()
+        else:
+            df.write.format("delta").partitionBy(partition_by).mode(
+                "overwrite").save(current_table_path)
+
+        df_history = df.withColumn("record_timestamp", F.current_timestamp())
+
+        if DeltaTable.isDeltaTable(self.spark, history_table_path):
+            df_history.write.format("delta").mode("append").partitionBy(
+                partition_by).save(history_table_path)
+        else:
+
+            df_history.write.format("delta").partitionBy(
+                partition_by).mode("overwrite").save(history_table_path)
